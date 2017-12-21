dd3b9519b643f51e9b7bb73ad7a6b1552021521f
LehiChen <hello.world.chen6599@gmail.com>
2017-12-21 15:06:40 +0800

    adapting2

--- spider.py
+++ spider.py
@@ -1,14 +1,14 @@
 import re,time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-url = "https://music.163.com/playlist?id=462072098"
+url = "https://music.163.com/playlist?id=453316317"
 #driver = webdriver.PhantomJS(executable_path="phantomjs-2.1.1-windows//bin//phantomjs")
 driver = webdriver.Chrome(executable_path="chromedriver")
 driver.get(url)
 driver.switch_to.frame(driver.find_element_by_xpath("//iframe"))
 trs = driver.find_elements_by_css_selector('span a[href]')
 trs_bad = driver.find_elements_by_css_selector('a[hidefocus="true"]')
-for i in range(64,432):
+for i in range(1,600):
 	try:
 		print(i)
 		if trs[i] not in trs_bad:
